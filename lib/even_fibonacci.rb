bdec1f00d4658418c20a4a67c1eda0e886cb31d7
onyoo <roberto9m9@gmail.com>
2015-11-02 18:30:04 +0100

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+
+# puts "Please define limit (the upper bound of your range):"
+# bound = puts.strip.to_i
+
+#creates an Fibonacci array that never reaches 4 mil.
+def even_fibonacci_sum(bound)
+  n = 2
+  old = 1
+  set = [1,2]
+  while n + old < bound
+    old, n = n, old + n
+    set << n
+  end
+  set.select{|num| num%2 == 0}.reduce(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    @n = 2
+    @old = 1
+    @set = [1,2]
+    while @n + @old < @limit
+      @old, @n = @n, @old + @n
+      @set << @n
+    end
+    @set.select{|num| num%2 == 0}.reduce(:+)
+  end
+end
